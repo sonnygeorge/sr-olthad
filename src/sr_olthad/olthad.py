3ace2edfe546982ada9e01b3648e6bc3ae74ae10
sonnygeorge <sonnygeorge5@gmail.com>
2025-03-12 18:27:28 -0400

    Add `iter_in_progress_descendants` method to `TaskNode`

--- src/sr_olthad/olthad.py
+++ src/sr_olthad/olthad.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Dict, List, Optional
+from typing import Dict, Generator, List, Optional, Tuple
 
 from pydantic import BaseModel
 
@@ -12,6 +12,47 @@ class TaskNode(BaseModel):
     status: TaskStatus
     retrospective: Optional[str] = None
     subtasks: Optional[List["TaskNode"]] = None
+
+    def iter_in_progress_descendants(
+        self,
+    ) -> Generator[Tuple["TaskNode", "TaskNode"], None, None]:
+        if self.status != TaskStatus.IN_PROGRESS:
+            raise ValueError(
+                "This method should only be called if the node is in-progress"
+            )
+
+        rebuild_root = TaskNode(
+            task=self.task,
+            description=self.description,
+            status=self.status,
+            retrospective=self.retrospective,
+            subtasks=None,
+        )
+
+        cur_rebuild_node = rebuild_root
+        cur_self_node = self
+        while True:
+            yield rebuild_root, cur_rebuild_node
+            cur_rebuild_node.subtasks = []
+            idx_of_in_progress_subtask = None
+            for i, subtask in enumerate(cur_self_node.subtasks):
+                rebuilt_subtask = TaskNode(
+                    task=subtask.task,
+                    description=subtask.description,
+                    status=subtask.status,
+                    retrospective=subtask.retrospective,
+                    subtasks=None,
+                )
+                cur_rebuild_node.subtasks.append(rebuilt_subtask)
+
+                if rebuilt_subtask.status == TaskStatus.IN_PROGRESS:
+                    idx_of_in_progress_subtask = i
+
+            if idx_of_in_progress_subtask is None:
+                break
+            else:
+                cur_self_node = cur_self_node.subtasks[idx_of_in_progress_subtask]
+                cur_rebuild_node = cur_rebuild_node.subtasks[idx_of_in_progress_subtask]
 
 
 @dataclass
