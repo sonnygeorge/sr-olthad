010270856c9378a1b93c989fbba5062b186d4f64
sonnygeorge <sonnygeorge5@gmail.com>
2025-03-12 14:54:53 -0400

    Update sr-OLTHAD config fields

--- src/sr_olthad/config.py
+++ src/sr_olthad/config.py
@@ -2,8 +2,34 @@ from enums import SerializationMethod
 
 STRINGIFY_ENV_STATE_SERIALIZATION_METHOD: SerializationMethod = SerializationMethod.YAML
 
-# Max tries for agents
-MAX_BACKTRACKER_TRIES: int = 2
-MAX_ATTEMPT_SUMMARIZER_TRIES: int = 3
-MAX_PLANNER_TRIES: int = 2
 
+class MaxValidOutputTries:
+    """Max tries to get a valid output for sr-OLTHAD `InstructLmAgents`."""
+
+    # Attempt summarizer
+    ATTEMPT_SUMMARIZER: int = 3
+    # Backtracker
+    EXHAUSTIVE_EFFORT_CLF: int = 2
+    MOST_WORTHWHILE_PURSUIT_CLF: int = 2
+    PARTIAL_SUCCESS_CLF: int = 2
+    SUCCESSFUL_COMPLETION_CLF: int = 2
+    # Forgetter
+    FORGETTER: int = 2
+    # Planner
+    PLANNER: int = 2
+
+
+class PromptVersions:
+    """Prompt versions to use for sr-OLTHAD `InstructLmAgents`."""
+
+    # Attempt summarizer
+    ATTEMPT_SUMMARIZER: str = "1.0"
+    # Backtracker
+    EXHAUSTIVE_EFFORT_CLF: str = "1.0"
+    MOST_WORTHWHILE_PURSUIT_CLF: str = "1.0"
+    PARTIAL_SUCCESS_CLF: str = "1.0"
+    SUCCESSFUL_COMPLETION_CLF: str = "1.0"
+    # Forgetter
+    FORGETTER: str = "1.0"
+    # Planner
+    PLANNER: str = "1.0"
