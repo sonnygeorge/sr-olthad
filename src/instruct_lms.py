699999d42c3f87c5eb5b2d600cd7117b133677b2
sonnygeorge <sonnygeorge5@gmail.com>
2025-03-12 11:32:18 -0400

    Add openai type hints

--- src/instruct_lms.py
+++ src/instruct_lms.py
@@ -2,11 +2,12 @@ import os
 from typing import Any, Callable, List, Optional
 
 from openai import AsyncOpenAI
+from openai.types.chat import ChatCompletion, ChatCompletionChunk
+
 
 from schema import InstructLm, InstructLmMessage
 
 
-# TODO: Add type-hinting w/ openai types
 class OpenAIInstructLm(InstructLm):
     def __init__(self, api_key: Optional[str] = None, model: str = "gpt-3.5-turbo"):
         api_key = os.getenv("OPENAI_API_KEY") if api_key is None else api_key
@@ -26,13 +27,14 @@ class OpenAIInstructLm(InstructLm):
             )
             full_response = ""
             async for chunk in response_generator:
+                chunk: ChatCompletionChunk
                 if chunk.choices and chunk.choices[0].delta.content is not None:
                     chunk_text = chunk.choices[0].delta.content
                     stream_handler(chunk_text)
                     full_response += chunk_text
             return full_response
         else:  # No need to stream
-            chat_completion = await self.client.chat.completions.create(
+            chat_completion: ChatCompletion = await self.client.chat.completions.create(
                 model=self.model, messages=messages, **kwargs
             )
             return chat_completion.choices[0].message.content
