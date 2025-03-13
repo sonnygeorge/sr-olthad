954cfe3899a87de3d5ca453d4cc6dfaca91c1da3
sonnygeorge <sonnygeorge5@gmail.com>
2025-03-12 23:19:14 -0400

    Light refactor I forgot to stage w/ last commit

--- src/sr_olthad/agents/backtracker/prompts/__init__.py
+++ src/sr_olthad/agents/backtracker/prompts/__init__.py
@@ -2,8 +2,8 @@ from sr_olthad.agents.backtracker.prompts.exhaustive_effort import (
     PROMPT_REGISTRY as EXHAUSTIVE_EFFORT_CLF_PROMPT_REGISTRY,
 )
 from sr_olthad.agents.backtracker.prompts.most_worthwhile_pursuit import (
-    PROMPT_REGISTRY as WORTHWHILE_PURSUIT_CLF_PROMPT_REGISTRY,
-    SysPromptInsertions as WorthwhilePursuitClfSysPromptInsertion,
+    PROMPT_REGISTRY as MOST_WORTHWHILE_PURSUIT_CLF_PROMPT_REGISTRY,
+    SysPromptInsertions as MostWorthwhilePursuitClfSysPromptInsertion,
 )
 from sr_olthad.agents.backtracker.prompts.partial_success import (
     PROMPT_REGISTRY as PARTIAL_SUCCESS_CLF_PROMPT_REGISTRY,
