45e03263f6f4386ae13bf98bdc7ec768cbff85ee
Ryan Echols <rkechols21@gmail.com>
2025-04-05 21:07:00 -0600

    fix circular import

--- src/sr_olthad/__init__.py
+++ src/sr_olthad/__init__.py
@@ -1,8 +1,8 @@
 from sr_olthad.lm_step import (
-    GetDomainSpecificInsert,
     PostLmStepApprover,
     PreLmStepHandler,
 )
 from sr_olthad.sr_olthad import SrOlthad
+from sr_olthad.utils import GetDomainSpecificInsert
 
 # TODO: Import other stuff like important types that users should know about?
