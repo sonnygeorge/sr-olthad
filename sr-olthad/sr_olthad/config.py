efd574f28dffa92816e7714896d80a6ac7e42ed3
Rocco A <43506958+GenerelSchwerz@users.noreply.github.com>
2025-04-16 23:31:40 -0400

    good

--- sr-olthad/sr_olthad/config.py
+++ sr-olthad/sr_olthad/config.py
@@ -1,6 +1,6 @@
 from typing import Protocol
 
-from sr_olthad.framework.lms import GroqInstructLm, OpenAIInstructLm
+from sr_olthad.framework.lms import GroqInstructLm, OpenAIInstructLm, HuggingFaceInstructLm, DeepSeekInstructLm
 from sr_olthad.framework.schema import InstructLm
 
 # General config
@@ -21,7 +21,7 @@ class LmAgentConfig(Protocol):
 
 class AttemptSummarizerCfg:
     MAX_TRIES_TO_GET_VALID_LM_RESPONSE: int = 5
-    INSTRUCT_LM: InstructLm = OpenAIInstructLm(model="gpt-4o-mini-2024-07-18")
+    INSTRUCT_LM: InstructLm = GroqInstructLm(model="llama-3.1-8b-instant")#GroqInstructLm(model="llama-3.1-8b-instant")#GroqInstructLm(model="llama-3.1-8b-instant")
     PROMPTS_VERSION = "1.0"
 
 
@@ -30,38 +30,38 @@ class BacktrackerCfg:
         N_CALLS_FOR_VOTING: int = 1
         MAX_ASYNC_CALLS_FOR_VOTING: int = 5
         MAX_TRIES_TO_GET_VALID_LM_RESPONSE: int = 7
-        INSTRUCT_LM: InstructLm = GroqInstructLm(model="llama-3.1-8b-instant")
+        INSTRUCT_LM: InstructLm =  GroqInstructLm(model="llama-3.1-8b-instant")#GroqInstructLm(model="llama-3.1-8b-instant")
         PROMPTS_VERSION = "1.0"
 
     class MostWorthwhilePursuitClfCfg:
         N_CALLS_FOR_VOTING: int = 1
         MAX_ASYNC_CALLS_FOR_VOTING: int = 5
         MAX_TRIES_TO_GET_VALID_LM_RESPONSE: int = 5
-        INSTRUCT_LM: InstructLm = OpenAIInstructLm(model="gpt-4o-mini-2024-07-18")
+        INSTRUCT_LM: InstructLm = GroqInstructLm(model="llama-3.1-8b-instant")#GroqInstructLm(model="llama-3.1-8b-instant")
         PROMPTS_VERSION = "1.0"
 
     class PartialSuccessClfCfg:
         N_CALLS_FOR_VOTING: int = 1
         MAX_ASYNC_CALLS_FOR_VOTING: int = 5
         MAX_TRIES_TO_GET_VALID_LM_RESPONSE: int = 7
-        INSTRUCT_LM: InstructLm = GroqInstructLm(model="llama-3.1-8b-instant")
+        INSTRUCT_LM: InstructLm =  GroqInstructLm(model="llama-3.1-8b-instant")#GroqInstructLm(model="llama-3.1-8b-instant")
         PROMPTS_VERSION = "1.0"
 
     class SuccessfulCompletionClfCfg:
         N_CALLS_FOR_VOTING: int = 1
         MAX_ASYNC_CALLS_FOR_VOTING: int = 5
         MAX_TRIES_TO_GET_VALID_LM_RESPONSE: int = 7
-        INSTRUCT_LM: InstructLm = GroqInstructLm(model="llama-3.1-8b-instant")
+        INSTRUCT_LM: InstructLm =  GroqInstructLm(model="llama-3.1-8b-instant")#GroqInstructLm(model="llama-3.1-8b-instant")
         PROMPTS_VERSION = "1.0"
 
 
 class ForgetterCfg:
     MAX_TRIES_TO_GET_VALID_LM_RESPONSE: int = 5
-    INSTRUCT_LM: InstructLm = OpenAIInstructLm(model="gpt-4o-mini-2024-07-18")
+    INSTRUCT_LM: InstructLm = GroqInstructLm(model="llama-3.1-8b-instant")#GroqInstructLm(model="llama-3.1-8b-instant")
     PROMPTS_VERSION = "1.0"
 
 
 class PlannerCfg:
     MAX_TRIES_TO_GET_VALID_LM_RESPONSE: int = 5
-    INSTRUCT_LM: InstructLm = OpenAIInstructLm(model="gpt-4o-mini-2024-07-18")
+    INSTRUCT_LM: InstructLm = GroqInstructLm(model="llama-3.1-8b-instant")#GroqInstructLm(model="llama-3.1-8b-instant")
     PROMPTS_VERSION = "1.0"
